15a1cac0c7de6f515e24993eaeca929f75353c8e
Erik Jansson <jansson_erik_@hotmail.com>
2017-12-04 17:28:10 +0100

    Added day 4

---
+++ src/2017/Jansson.AdventOfCode.2017/Jansson.AdventOfCode.2017.Day4.Tests/Day4Tests.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Jansson.AdventOfCode._2017.Day4.Tests
+{
+    [TestFixture]
+    public class Day4Tests
+    {
+        private PassphraseValidator _passphraseValidator;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            _passphraseValidator = new PassphraseValidator();
+        }
+
+        [Test]
+        public void Should_return_valid_count_test_input_part1()
+        {
+            _passphraseValidator.CalculateValidPassphraseCountWordLimit(@"C:\dev\Jansson.AdventOfCode\src\2017\Jansson.AdventOfCode.2017\Jansson.AdventOfCode.2017.Day4.Tests\Part1TestInput.txt").Should().Be(2);
+        }
+
+        [Test]
+        public void Should_return_result_count_part1()
+        {
+            _passphraseValidator.CalculateValidPassphraseCountWordLimit(@"C:\dev\Jansson.AdventOfCode\src\2017\Jansson.AdventOfCode.2017\Jansson.AdventOfCode.2017.Day4.Tests\InputFile.txt").Should().Be(337);
+        }
+
+        [Test]
+        public void Should_return_valid_count_test_input_part2()
+        {
+            _passphraseValidator.CalculateValidPassphraseCountWordLimitWithAnagram(@"C:\dev\Jansson.AdventOfCode\src\2017\Jansson.AdventOfCode.2017\Jansson.AdventOfCode.2017.Day4.Tests\Part2TestInput.txt").Should().Be(3);
+        }
+
+        [Test]
+        public void Should_return_result_count_part2()
+        {
+            _passphraseValidator.CalculateValidPassphraseCountWordLimitWithAnagram(@"C:\dev\Jansson.AdventOfCode\src\2017\Jansson.AdventOfCode.2017\Jansson.AdventOfCode.2017.Day4.Tests\InputFile.txt").Should().Be(231);
+        }
+    }
+}
---
+++ src/2017/Jansson.AdventOfCode.2017/Jansson.AdventOfCode.2017.Day4/PassphraseValidator.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Jansson.AdventOfCode._2017.Day4
+{
+    public class PassphraseValidator
+    {
+        public int CalculateValidPassphraseCountWordLimit(string fileName)
+        {
+            var passphrases = ParsePassphrases(fileName);
+
+            return passphrases.Count(x => x.IsValid());
+        }
+
+        private static IEnumerable<Passphrase> ParsePassphrases(string fileName)
+        {
+            return File
+                .ReadAllLines(fileName)
+                .Select(x => new Passphrase(x))
+                .ToList();
+        }
+
+        public int CalculateValidPassphraseCountWordLimitWithAnagram(string fileName)
+        {
+            var passphrases = ParsePassphrases(fileName);
+
+            return passphrases.Count(x => x.IsValidAnagramStyle());
+        }
+    }
+
+    public class Passphrase
+    {
+        public Passphrase(string words)
+        {
+            Words = words.Split(null);
+        }
+
+        public string[] Words { get; set; }
+
+        public bool IsValid()
+        {
+            return !Words
+                .GroupBy(x => x)
+                .Any(x => x.Count() > 1);
+        }
+
+        public bool IsValidAnagramStyle()
+        {
+            return !Words
+                .GroupBy(x => string.Concat(x.OrderBy(s => s)))
+                .Any(x => x.Count() > 1);
+        }
+    }
+}
