b3b586d6e3312c42e4d0bf0419347612c40f8e31
Erik Jansson <jansson_erik_@hotmail.com>
2018-12-01 08:40:59 +0100

    Added day 1 of 2018.

---
+++ src/2018/Jansson.AdventOfCode.2018/Jansson.AdventOfCode.2018.Day1.Tests/Day1Tests.cs
@@ -0,0 +1,65 @@
+﻿using System.IO;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Jansson.AdventOfCode._2018.Day1.Tests
+{
+    [TestFixture]
+    public class Day1Tests
+    {
+        private TimeTravelFrequencyCalculator _frequencyCalculator;
+
+        [OneTimeSetUp]
+        public void OneTImeSetUp()
+        {
+            _frequencyCalculator = new TimeTravelFrequencyCalculator();
+        }
+
+        [Test]
+        [TestCase("Day1Part1TestFIle1.txt", 3)]
+        [TestCase("Day1Part1TestFIle2.txt", 0)]
+        [TestCase("Day1Part1TestFIle3.txt", -6)]
+        public void Day1_testInput_part1(string filePath, int expectedScore)
+        {
+            var input = File.ReadAllLines(filePath);
+
+            var result = _frequencyCalculator.CalculateDeviceFrequency(input);
+
+            result.Should().Be(expectedScore);
+        }
+
+        [Test]
+        public void Day1_result_part1()
+        {
+            var input = File.ReadAllLines("Day1Part1Input.txt");
+
+            var result = _frequencyCalculator.CalculateDeviceFrequency(input);
+
+            result.Should().Be(510);
+        }
+
+
+        [Test]
+        [TestCase("Day1Part2TestFIle1.txt", 2)]
+        [TestCase("Day1Part2TestFIle2.txt", 10)]
+        [TestCase("Day1Part2TestFIle3.txt", 5)]
+        public void Day1_testInput_part2(string filePath, int expectedScore)
+        {
+            var input = File.ReadAllLines(filePath);
+
+            var result = _frequencyCalculator.CalculateDeviceFrequencyToFindDuplicates(input);
+
+            result.Should().Be(expectedScore);
+        }
+
+        [Test]
+        public void Day1_result_part2()
+        {
+            var input = File.ReadAllLines("Day1Part1Input.txt");
+
+            var result = _frequencyCalculator.CalculateDeviceFrequencyToFindDuplicates(input);
+
+            result.Should().Be(69074);
+        }
+    }
+}
---
+++ src/2018/Jansson.AdventOfCode.2018/Jansson.AdventOfCode.2018.Day1/TimeTravelFrequencyCalculator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Jansson.AdventOfCode._2018.Day1
+{
+    public class TimeTravelFrequencyCalculator
+    {
+        public int CalculateDeviceFrequency(string[] input)
+        {
+            return input
+                .Select(int.Parse)
+                .Sum();
+        }
+
+        public int CalculateDeviceFrequencyToFindDuplicates(string[] input)
+        {
+            var log = new List<int> {0};
+            var frequencyChanges = input.Select(int.Parse).ToList();
+            var currentValue = 0;
+
+            while (true)
+                foreach (var number in frequencyChanges)
+                {
+                    currentValue += number;
+
+
+                    if (log.Any(i => i == currentValue))
+                        return currentValue;
+
+                    log.Add(currentValue);
+                }
+        }
+    }
+}
