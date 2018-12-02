a4d071d99041b41f5f6358d625b0c57d8a70d143
Erik Jansson <jansson_erik_@hotmail.com>
2018-12-02 23:13:20 +0100

    Added day 2

---
+++ src/2018/Jansson.AdventOfCode.2018/Jansson.AdventOfCode.2018.Day2.Tests/Day2Tests.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Jansson.AdventOfCode._2018.Day2.Tests
+{
+    [TestFixture]
+    public class Day2Tests
+    {
+        private BoxChecksumCalculator _calculator;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            _calculator = new BoxChecksumCalculator();
+        }
+
+        [Test]
+        public void Day2_testInput_part1()
+        {
+            var input = File.ReadAllLines("Day2Part1TestFile1.txt");
+
+            var result = _calculator.CalculateChecksum(input);
+
+            result.Should().Be(12);
+        }
+
+        [Test]
+        public void Day2_result_part1()
+        {
+            var input = File.ReadAllLines("Day2Input.txt");
+
+            var result = _calculator.CalculateChecksum(input);
+
+            result.Should().Be(3952);
+        }
+
+        [Test]
+        public void Day2_testInput_part2()
+        {
+            var input = File.ReadAllLines("Day2Part2TestFile1.txt");
+
+            var result = _calculator.GetCommonCharactersOfSingleDifferingId(input);
+
+            result.Should().Be("fgij");
+        }
+
+        [Test]
+        public void Day2_result_part2()
+        {
+            var input = File.ReadAllLines("Day2Input.txt");
+
+            var result = _calculator.GetCommonCharactersOfSingleDifferingId(input);
+
+            result.Should().Be("vtnikorkulbfejvyznqgdxpaw");
+        }
+    }
+}
---
+++ src/2018/Jansson.AdventOfCode.2018/Jansson.AdventOfCode.2018.Day2/BoxChecksumCalculator.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Jansson.AdventOfCode._2018.Day2
+{
+    public class BoxChecksumCalculator
+    {
+        public int CalculateChecksum(string[] boxIds)
+        {
+            var twos = 0;
+            var threes = 0;
+
+            foreach (var boxId in boxIds)
+            {
+                var counts = boxId
+                    .GroupBy(c => c)
+                    .Select(g => g.Count())
+                    .ToList();
+
+                if (counts.Any(c => c == 2))
+                    twos++;
+
+                if (counts.Any(c => c == 3))
+                    threes++;
+            }
+
+            return twos*threes;
+        }
+
+        public string GetCommonCharactersOfSingleDifferingId(string[] boxIds)  
+        {
+            var @checked = new List<string>();
+
+            foreach (var boxId in boxIds)
+            {
+                foreach (var old in @checked)
+                {
+                    if (DiffersByOne(boxId, old, out var common))
+                        return common;
+                }
+
+                @checked.Add(boxId);
+            }
+
+            return "";
+        }
+
+        private bool DiffersByOne(string one, string two, out string common)
+        {
+            common = "";
+
+            if (one.Length != two.Length)
+            {
+                return false;
+            }
+
+            int counter = 0;
+            int differIndex = 0;
+            for (var i = 0; i < one.Length; i++)
+            {
+                if (one[i] != two[i])
+                {
+                    counter++;
+                    differIndex = i;
+                }
+            }
+            if (counter > 1)
+            {
+                return false;
+            }
+
+            common = one.Remove(differIndex, 1);
+            return true;
+        }
+    }
+}
