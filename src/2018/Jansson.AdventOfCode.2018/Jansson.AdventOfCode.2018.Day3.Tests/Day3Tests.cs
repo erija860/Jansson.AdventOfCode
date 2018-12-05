536ff14d8fc5d0f648531856da6e3e9b378fdac0
Erik Jansson <jansson_erik_@hotmail.com>
2018-12-05 22:09:07 +0100

    Added day 3

---
+++ src/2018/Jansson.AdventOfCode.2018/Jansson.AdventOfCode.2018.Day3.Tests/Day3Tests.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Jansson.AdventOfCode._2018.Day3.Tests
+{
+    [TestFixture]
+    public class Day3Tests
+    {
+        private FabricCalculator _calculator;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            _calculator = new FabricCalculator();
+        }
+
+        [Test]
+        public void Day3Part1TestInput()
+        {
+            var input = File.ReadAllLines("Day3Part1TestFile1.txt");
+
+            var result = _calculator.CalculateOverlappingSquareInches(input);
+
+            result.Should().Be(4);
+        }
+
+        [Test]
+        public void Day3Part1Result()
+        {
+            var input = File.ReadAllLines("Day3Input.txt");
+
+            var result = _calculator.CalculateOverlappingSquareInches(input);
+
+            result.Should().Be(110383);
+        }
+
+        [Test]
+        public void Day3Part2TestInput()
+        {
+            var input = File.ReadAllLines("Day3Part1TestFile1.txt");
+
+            var result = _calculator.FindNonOverlappingClaim(input);
+
+            result.Should().Be("3");
+        }
+
+        [Test]
+        public void Day3Part2Result()
+        {
+            var input = File.ReadAllLines("Day3Input.txt");
+
+            var result = _calculator.FindNonOverlappingClaim(input);
+
+            result.Should().Be("129");
+        }
+    }
+}
---
+++ src/2018/Jansson.AdventOfCode.2018/Jansson.AdventOfCode.2018.Day3/FabricCalculator.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace Jansson.AdventOfCode._2018.Day3
+{
+    public class FabricCalculator
+    {
+        public int CalculateOverlappingSquareInches(string[] input)
+        {
+            var claims = input.Select(SantaSuitClaim.Create).ToList();
+            var fabric = new Dictionary<Point, int>();
+            claims.ForEach(c=>c.AddClaimArea(fabric));
+
+            return fabric.Count(c=>c.Value>1);
+        }
+
+        public string FindNonOverlappingClaim(string[] input)
+        {
+            var claims = input.Select(SantaSuitClaim.Create).ToList();
+            var fabric = new Dictionary<Point, int>();
+            claims.ForEach(c => c.AddClaimArea(fabric));
+
+            return claims.FirstOrDefault(c => !c.Overlaps(fabric))?.Id;
+        }
+
+        private class SantaSuitClaim
+        {
+            public string Id { get; set; }
+            public int LeftPadding { get; set; }
+            public int TopPadding { get; set; }
+            public int Width { get; set; }
+            public int Height { get; set; }
+
+            public static SantaSuitClaim Create(string input)
+            {
+                input = input
+                    .Replace("@ ", "")
+                    .Replace(":", "")
+                    .Replace("#", "");
+
+                var split = input.Split(null);
+
+                return new SantaSuitClaim
+                {
+                    Id = split[0],
+                    LeftPadding = int.Parse(split[1].Split(",")[0]),
+                    TopPadding = int.Parse(split[1].Split(",")[1]),
+                    Width = int.Parse(split[2].Split("x")[0]),
+                    Height = int.Parse(split[2].Split("x")[1])
+                };
+            }
+
+            public void AddClaimArea(Dictionary<Point, int> fabric)
+            {
+                for (var i = LeftPadding; i < LeftPadding + Width; i++)
+                {
+                    for (var j = TopPadding; j < TopPadding + Height; j++)
+                    {
+                        var squareInch = fabric.GetValueOrDefault(new Point(i, j));
+                        fabric[new Point(i, j)] = ++squareInch;
+                    }
+                }
+            }
+
+            public bool Overlaps(Dictionary<Point, int> fabric)
+            {
+                for (var i = LeftPadding; i < LeftPadding + Width; i++)
+                {
+                    for (var j = TopPadding; j < TopPadding + Height; j++)
+                    {
+                        var squareInch = fabric.GetValueOrDefault(new Point(i, j));
+                        if (squareInch>1)
+                        {
+                            return true;
+                        }
+                    }
+                }
+
+                return false;
+            }
+        }
+    }
+}
